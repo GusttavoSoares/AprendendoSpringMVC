1336d74c65e5dee19bdc4b6690b6a15c8c00cf07
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-10 10:57:03 -0300

    Como adicionar objetos nas requisições e como apresentá-los de maneira mais fácil. Primeiros passos com thymeleaf.

--- src/main/java/br/com/ifsp/regescweb/controllers/HelloController.java
+++ src/main/java/br/com/ifsp/regescweb/controllers/HelloController.java
@@ -1,14 +1,34 @@
 package br.com.ifsp.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
+
     @GetMapping("/hello")
-    public String hello() {
-        System.out.println("*************");
-        return "hello"; // Spring renderiza o arquivo que está em templates/hello.html
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello"); // nome do arquivo html a ser renderizado
+        mv.addObject("nome", "José!");
+        return mv; // Spring renderiza o arquivo que está em templates/hello.html
     }
+
+    @GetMapping("/hello-model")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Zezinho");
+        return "hello";  // Spring renderiza o arquivo que está em templates/hello.html
+    }
+
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Samuka");
+        return "hello";  // Spring renderiza o arquivo que está em templates/hello.html
+        // Lembrando que esse hello.html renderizado está somente no lado servidor
+    }
+
 }
