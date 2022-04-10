9a7b5a67d14f502e7c85edd65b5d4cc3419e1aec
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-10 11:27:45 -0300

    Criando a entidade Professor e colocando os atributos e métodos

---
+++ src/main/java/br/com/ifsp/regescweb/models/Professor.java
@@ -0,0 +1,53 @@
+package br.com.ifsp.regescweb.models;
+
+import com.sun.istack.NotNull;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long iD;
+    @Column (nullable = false)
+    private String nome;
+    private BigDecimal salario;
+    @Enumerated(EnumType.STRING)
+    private StatusProfessor statusProfessor;
+
+    public Professor(){}
+
+    public Long getiD() {
+        return iD;
+    }
+
+    public void setiD(Long iD) {
+        this.iD = iD;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public StatusProfessor getStatusProfessor() {
+        return statusProfessor;
+    }
+
+    public void setStatusProfessor(StatusProfessor statusProfessor) {
+        this.statusProfessor = statusProfessor;
+    }
+}
---
+++ src/main/java/br/com/ifsp/regescweb/models/StatusProfessor.java
@@ -0,0 +1,5 @@
+package br.com.ifsp.regescweb.models;
+
+public enum StatusProfessor {
+    ATIVO, INATIVO, AFASTADO, APOSENTADO;
+}
