016cc01aae9328bb7cce4c95a93066713e956d53
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-13 10:04:33 -0300

    Aprendendo como evitar o Web Parameter Tampering e como fazer a persistência das informações preenchidas na professor/new  no banco de dados.

--- src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
@@ -1,5 +1,6 @@
 package br.com.ifsp.regescweb.controllers;
 
+import br.com.ifsp.regescweb.dto.RequisicaoNovoProfessor;
 import br.com.ifsp.regescweb.models.Professor;
 import br.com.ifsp.regescweb.models.StatusProfessor;
 import br.com.ifsp.regescweb.repositories.ProfessorRepository;
@@ -34,11 +35,10 @@ public class ProfessorController {
         }
 
         @PostMapping ("/professores")
-        public String create(Professor professor){
+        public String create(RequisicaoNovoProfessor requisicao){
         // Para cada um dos atributos do formulário o spring coloca os valores nesse objeto (nome dos atributos devem ser iguais ao nome dos campos)
-            System.out.println();
-            System.out.println(professor);
-            System.out.println();
+            Professor professor = requisicao.toProfessor();
+            this.professorRepository.save(professor);
             return "redirect:/professores";
 
             /* HÁ UM PROBLEMA DE SEGURANÇA: Web Parameter Tampering
---
+++ src/main/java/br/com/ifsp/regescweb/dto/RequisicaoNovoProfessor.java
@@ -0,0 +1,55 @@
+package br.com.ifsp.regescweb.dto;
+
+import br.com.ifsp.regescweb.models.Professor;
+import br.com.ifsp.regescweb.models.StatusProfessor;
+
+import java.math.BigDecimal;
+
+// Classe DTO (Data Transfer Object)
+// Com essa classe é possível proteger o sistema de usuários que tentem colocar algum campo não esperado
+public class RequisicaoNovoProfessor {
+    private String nome;
+    private StatusProfessor statusProfessor;
+    private BigDecimal salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public StatusProfessor getStatusProfessor() {
+        return statusProfessor;
+    }
+
+    public void setStatusProfessor(StatusProfessor statusProfessor) {
+        this.statusProfessor = statusProfessor;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public Professor toProfessor(){
+        Professor professor = new Professor();
+        professor.setNome(this.nome);
+        professor.setSalario(this.salario);
+        professor.setStatusProfessor(this.statusProfessor);
+
+        return professor;
+    }
+    @Override
+    public String toString() {
+        return "RequisicaoNovoProfessor{" +
+                "nome='" + nome + '\'' +
+                ", statusProfessor=" + statusProfessor +
+                ", salario=" + salario +
+                '}';
+    }
+}
