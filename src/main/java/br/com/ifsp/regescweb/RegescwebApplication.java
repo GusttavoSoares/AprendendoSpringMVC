b35e2d71e5cbd6d26be30d9fb8e3deb5b018e908
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-16 19:05:14 -0300

    Fazendo a página de detalhes de uma entidade

--- src/main/java/br/com/ifsp/regescweb/RegescwebApplication.java
+++ src/main/java/br/com/ifsp/regescweb/RegescwebApplication.java
@@ -1,4 +1,4 @@
-package br.com.ifsp.regescweb;
+ package br.com.ifsp.regescweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
@@ -11,11 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -53,6 +55,24 @@ public class ProfessorController {
             // Para cada um dos atributos do formulário o spring coloca os valores nesse objeto (nome dos atributos devem ser iguais ao nome dos campos)
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor);
+            return new ModelAndView("redirect:/professores/" + professor.getiD());
+        }
+
+    }
+    @GetMapping("professores/{iD}")
+    public ModelAndView show(@PathVariable Long iD){
+        Optional <Professor> optional = this.professorRepository.findById(iD);
+
+        if (optional.isPresent()){
+            Professor professor = optional.get();
+
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+            return mv;
+        }
+        //Não achou um registro na tabela Professor com o id informado
+        else{
+            System.out.println("$$$$$$$$$$$$$$$$$ nao achou o professor de iD " +iD + "$$$$$$$$");
             return new ModelAndView("redirect:/professores");
         }
 
